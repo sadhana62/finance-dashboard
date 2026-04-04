96f26dffc5f1114f3bdaa3b8fab3ac8e1e2b758c
sadhana62 <sadhana.19582@sscbs.du.ac.in>
2026-04-04 22:09:40 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),  visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })],
+    build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+  },
 })
